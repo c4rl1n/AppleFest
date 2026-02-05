8785aee058b6f7d94fbda103c33b103c0ac292cc
c4rl1n <95320779+c4rl1n@users.noreply.github.com>
2026-02-04 21:20:51 -0500

    Add files via upload

---
+++ scripts/dropdownmenu.js
@@ -0,0 +1,84 @@
+$("#farmfresh-list").addClass("hidden");
+$("#ciderhouses-list").addClass("hidden");
+$("#foodvendors-list").addClass("hidden");
+$("#drinkvendors-list").addClass("hidden");
+$("#snackvendors-list").addClass("hidden");
+$(".duarrow").addClass("hidden");
+
+$("#ddarrow1").click(function() {
+    $("#ddarrow1").addClass("hidden");
+    $("#duarrow1").removeClass("hidden");
+    $("#farmfresh-list").removeClass("hidden");
+  });
+$("#ddarrow2").click(function() {
+    $("#ddarrow2").addClass("hidden");
+    $("#duarrow2").removeClass("hidden");
+   $("#ciderhouses-list").removeClass("hidden");
+  });
+$("#ddarrow3").click(function() {
+    $("#ddarrow3").addClass("hidden");
+    $("#duarrow3").removeClass("hidden");
+    $("#foodvendors-list").removeClass("hidden");
+});
+$("#ddarrow4").click(function() {
+    $("#ddarrow4").addClass("hidden");
+    $("#duarrow4").removeClass("hidden");
+    $("#drinkvendors-list").removeClass("hidden");
+});
+$("#ddarrow5").click(function() {
+    $("#ddarrow5").addClass("hidden");
+    $("#duarrow5").removeClass("hidden");
+    $("#snackvendors-list").removeClass("hidden");
+});
+
+$("#duarrow1").click(function() {
+    $("#farmfresh-list").addClass("hidden");
+    $("#ddarrow1").removeClass("hidden");
+    $("#duarrow1").addClass("hidden");
+  });
+$("#duarrow2").click(function() {
+   $("#ciderhouses-list").addClass("hidden");
+   $("#ddarrow2").removeClass("hidden");
+   $("#duarrow2").addClass("hidden");
+  });
+$("#duarrow3").click(function() {
+    $("#foodvendors-list").addClass("hidden");
+    $("#ddarrow3").removeClass("hidden");
+    $("#duarrow3").addClass("hidden");
+});
+$("#duarrow4").click(function() {
+    $("#drinkvendors-list").addClass("hidden");
+    $("#ddarrow4").removeClass("hidden");
+    $("#duarrow4").addClass("hidden");
+});
+$("#duarrow5").click(function() {
+    $("#snackvendors-list").addClass("hidden");
+    $("#ddarrow5").removeClass("hidden");
+    $("#duarrow5").addClass("hidden");
+});
+
+$("#ddarrow1").click(function() {
+    $("#ddarrow1").addClass("hidden");
+    $("#duarrow1").removeClass("hidden");
+    $("#farmfresh-list").removeClass("hidden");
+  });
+$("#ddarrow2").click(function() {
+    $("#ddarrow2").addClass("hidden");
+    $("#duarrow2").removeClass("hidden");
+   $("#ciderhouses-list").removeClass("hidden");
+  });
+$("#ddarrow3").click(function() {
+    $("#ddarrow3").addClass("hidden");
+    $("#duarrow3").removeClass("hidden");
+    $("#foodvendors-list").removeClass("hidden");
+});
+$("#ddarrow4").click(function() {
+    $("#ddarrow4").addClass("hidden");
+    $("#duarrow4").removeClass("hidden");
+    $("#drinkvendors-list").removeClass("hidden");
+});
+$("#ddarrow5").click(function() {
+    $("#ddarrow5").addClass("hidden");
+    $("#duarrow5").removeClass("hidden");
+    $("#snackvendors-list").removeClass("hidden");
+});
---
+++ scripts/gallery.js
@@ -0,0 +1,207 @@
+$("#back").addClass("hidden");
+$(".applemodal").addClass("hidden");
+$(".caption").addClass("hidden");
+
+    $("#pic1").click(function() {
+    $(".applestuff").addClass("hidden");
+    $(".rowone").addClass("hidden");
+    $(".rowtwo").addClass("hidden");
+    $(".rowthree").addClass("hidden");
+    $(".rowfour").addClass("hidden");
+    $("#pic1m").removeClass("hidden");
+    $("#pic1m").addClass("enlarge");
+    $("#cap1").removeClass("hidden");
+    $("#back").removeClass("hidden");
+
+});
+
+  $("#pic2").click(function() {
+    $(".applestuff").addClass("hidden");
+    $(".rowone").addClass("hidden");
+    $(".rowtwo").addClass("hidden");
+    $(".rowthree").addClass("hidden");
+    $(".rowfour").addClass("hidden");
+    $("#pic2m").removeClass("hidden");
+    $("#pic2m").addClass("enlarge");
+    $("#cap2").removeClass("hidden");
+    $("#back").removeClass("hidden");
+  });
+
+  $("#pic3").click(function() {
+    $(".applestuff").addClass("hidden");
+    $(".rowone").addClass("hidden");
+    $(".rowtwo").addClass("hidden");
+    $(".rowthree").addClass("hidden");
+    $(".rowfour").addClass("hidden");
+    $("#pic3m").removeClass("hidden");
+    $("#pic3m").addClass("enlarge");
+    $("#cap3").removeClass("hidden");
+    $("#back").removeClass("hidden");
+  });
+
+  $("#pic4").click(function() {
+    $(".applestuff").addClass("hidden");
+    $(".rowone").addClass("hidden");
+    $(".rowtwo").addClass("hidden");
+    $(".rowthree").addClass("hidden");
+    $(".rowfour").addClass("hidden");
+    $("#pic4m").removeClass("hidden");
+    $("#pic4m").addClass("enlarge");
+    $("#cap4").removeClass("hidden");
+    $("#back").removeClass("hidden");
+  });
+
+  $("#pic5").click(function() {
+    $(".applestuff").addClass("hidden");
+    $(".rowone").addClass("hidden");
+    $(".rowtwo").addClass("hidden");
+    $(".rowthree").addClass("hidden");
+    $(".rowfour").addClass("hidden");
+    $("#pic5m").removeClass("hidden");
+    $("#pic5m").addClass("enlarge");
+    $("#cap5").removeClass("hidden");
+    $("#back").removeClass("hidden");
+  });
+
+  $("#pic6").click(function() {
+    $(".applestuff").addClass("hidden");
+    $(".rowone").addClass("hidden");
+    $(".rowtwo").addClass("hidden");
+    $(".rowthree").addClass("hidden");
+    $(".rowfour").addClass("hidden");
+    $("#pic6m").removeClass("hidden");
+    $("#pic6m").addClass("enlarge");
+    $("#cap6").removeClass("hidden");
+    $("#back").removeClass("hidden");
+    });
+
+  $("#pic7").click(function() {
+    $(".applestuff").addClass("hidden");
+    $(".rowone").addClass("hidden");
+    $(".rowtwo").addClass("hidden");
+    $(".rowthree").addClass("hidden");
+    $(".rowfour").addClass("hidden");
+    $("#pic7m").removeClass("hidden");
+    $("#pic7m").addClass("enlarge");
+    $("#cap7").removeClass("hidden");
+    $("#back").removeClass("hidden");
+    });
+
+  $("#pic8").click(function() {
+    $(".applestuff").addClass("hidden");
+    $(".rowone").addClass("hidden");
+    $(".rowtwo").addClass("hidden");
+    $(".rowthree").addClass("hidden");
+    $(".rowfour").addClass("hidden");
+    $("#pic8m").removeClass("hidden");
+    $("#pic8m").addClass("enlarge");
+    $("#cap8").removeClass("hidden");
+    $("#back").removeClass("hidden");
+    });
+
+  $("#pic9").click(function() {
+    $(".applestuff").addClass("hidden");
+    $(".rowone").addClass("hidden");
+    $(".rowtwo").addClass("hidden");
+    $(".rowthree").addClass("hidden");
+    $(".rowfour").addClass("hidden");
+    $("#pic9m").removeClass("hidden");
+    $("#pic9m").addClass("enlarge");
+    $("#cap9").removeClass("hidden");
+    $("#back").removeClass("hidden");
+    });
+
+  $("#pic10").click(function() {
+    $(".applestuff").addClass("hidden");
+    $(".rowone").addClass("hidden");
+    $(".rowtwo").addClass("hidden");
+    $(".rowthree").addClass("hidden");
+    $(".rowfour").addClass("hidden");
+    $("#pic10m").removeClass("hidden");
+    $("#pic10m").addClass("enlarge");
+    $("#cap10").removeClass("hidden");
+    $("#back").removeClass("hidden");
+  });
+
+  $("#pic11").click(function() {
+    $(".applestuff").addClass("hidden");
+    $(".rowone").addClass("hidden");
+    $(".rowtwo").addClass("hidden");
+    $(".rowthree").addClass("hidden");
+    $(".rowfour").addClass("hidden");
+    $("#pic11m").removeClass("hidden");
+    $("#pic11m").addClass("enlarge");
+    $("#cap11").removeClass("hidden");
+    $("#back").removeClass("hidden");
+    });
+
+  $("#pic12").click(function() {
+    $(".applestuff").addClass("hidden");
+    $(".rowone").addClass("hidden");
+    $(".rowtwo").addClass("hidden");
+    $(".rowthree").addClass("hidden");
+    $(".rowfour").addClass("hidden");
+    $("#pic12m").removeClass("hidden");
+    $("#pic12m").addClass("enlarge");
+    $("#cap12").removeClass("hidden");
+    $("#back").removeClass("hidden");
+    });
+
+  $("#pic13").click(function() {
+    $(".applestuff").addClass("hidden");
+    $(".rowone").addClass("hidden");
+    $(".rowtwo").addClass("hidden");
+    $(".rowthree").addClass("hidden");
+    $(".rowfour").addClass("hidden");
+    $("#pic13m").removeClass("hidden");
+    $("#pic13m").addClass("enlarge");
+    $("#cap13").removeClass("hidden");
+    $("#back").removeClass("hidden");
+    });
+
+  $("#pic14").click(function() {
+    $(".applestuff").addClass("hidden");
+    $(".rowone").addClass("hidden");
+    $(".rowtwo").addClass("hidden");
+    $(".rowthree").addClass("hidden");
+    $(".rowfour").addClass("hidden");
+    $("#pic14m").removeClass("hidden");
+    $("#pic14m").addClass("enlarge");
+    $("#cap14").removeClass("hidden");
+    $("#back").removeClass("hidden");
+    });
+
+  $("#pic15").click(function() {
+    $(".applestuff").addClass("hidden");
+    $(".rowone").addClass("hidden");
+    $(".rowtwo").addClass("hidden");
+    $(".rowthree").addClass("hidden");
+    $(".rowfour").addClass("hidden");
+    $("#pic15m").removeClass("hidden");
+    $("#pic15m").addClass("enlarge");
+    $("#cap15").removeClass("hidden");
+    $("#back").removeClass("hidden");
+    });
+
+  $("#pic16").click(function() {
+    $(".applestuff").addClass("hidden");
+    $(".rowone").addClass("hidden");
+    $(".rowtwo").addClass("hidden");
+    $(".rowthree").addClass("hidden");
+    $(".rowfour").addClass("hidden");
+    $("#pic16m").removeClass("hidden");
+    $("#pic16m").addClass("enlarge");
+    $("#cap16").removeClass("hidden");
+    $("#back").removeClass("hidden");
+    });
+
+  $("#back").click(function(){
+    $(".applestuff").removeClass("hidden");
+    $(".rowone").removeClass("hidden");
+    $(".rowtwo").removeClass("hidden");
+    $(".rowthree").removeClass("hidden");
+    $(".rowfour").removeClass("hidden");
+    $(".applemodal").addClass("hidden");
+    $(".caption").addClass("hidden");
+    $("#back").addClass("hidden");
+    });
